a0f358778ddf17fc3b7c77c257eb1fe32654c830
Thomas Blachowiak <blachowiak.thomas@outlook.fr>
2024-05-28 16:35:56 +0200

    add security for the home page

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from . import forms
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+
+from django.views.generic import View
+
+
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+        
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        message = 'Identifiants invalides.'
+        return render(request, self.template_name, context={'form': form, 'message': message})
 
 def login_page(request):
     form = forms.LoginForm()
@@ -15,8 +39,12 @@ def login_page(request):
             )
             if user is not None:
                 login(request, user)
-                message = f'Bonjour, {user.username}! Vous êtes connecté.'
-            else:
-                message = 'Identifiants invalides.'
+                return redirect('home')
+            message = 'Identifiants invalides.'
     return render(
         request, 'authentication/login.html', context={'form': form, 'message': message})
+    
+def logout_user(request):
+    
+    logout(request)
+    return redirect('login')

fb2a8ad9848dc2055c61b5468dfa378b473317fa
Thomas Blachowiak <blachowiak.thomas@outlook.fr>
2024-05-29 15:19:35 +0200

    create blog form and view

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -28,19 +28,23 @@ urlpatterns = [
     path('', LoginView.as_view(
         template_name='authentication/login.html',
         redirect_authenticated_user=True),
-         name='login'),
+        name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('change-password/', PasswordChangeView.as_view(
         template_name='authentication/password_change_form.html'),
-         name='password_change'
-         ),
+        name='password_change'
+        ),
     path('change-password-done/', PasswordChangeDoneView.as_view(
         template_name='authentication/password_change_done.html'),
-         name='password_change_done'
-         ),
+        name='password_change_done'
+        ),
     path('signup/', authentication.views.signup_page, name='signup'),
     path('home/', blog.views.home, name='home'),
     path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
+    path('profile-photo/upload', authentication.views.upload_profile_photo,
+        name='upload_profile_photo'),
+    path('blog/create', blog.views.blog_and_photo_upload, name='blog_create'),
+    path('blog/<int:blog_id>', blog.views.view_blog, name='view_blog'),
 ]
 
 if settings.DEBUG:
